921a6cd0d9446076facf1f0dd8f8f97997aeaa9b
Pranav Sahu <pranavsahu28@gmail.com>
2024-06-09 23:06:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import HowItWorks from './components/HowItWorks';
+import Tokenomics from './components/Tokenomics';
+
+import Footer from './components/Footer';
+import HeroSection2 from './components/HeroSection2';
+import Scroll from './components/Scroll';
+import Outfit from './components/Outfit';
+import Roadmap from './components/Roadmap';
+import GetStarted from './components/GetStarted';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <HeroSection2/>
+      {/* <AvatarGrid /> */}
+      <HowItWorks />
+      <Scroll/>
+      <Outfit/>
+      <Tokenomics />
+      <Roadmap/>
+      <GetStarted/>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
